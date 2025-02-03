4ec5a671844d8d0948ddd4e77dfe5f6db0f1c506
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-03 16:32:08 +0100

    add models

---
+++ app/Models/Employe.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Employe extends Model
+{
+    use HasFactory;
+
+    public function Enterprise (): BelongsTo
+    {
+        return $this->belongsTo(Enterprise::class);
+    }
+}
