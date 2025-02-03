a594c1ba26b8b0555b4c4cc0ea70861e4a848a7b
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-03 14:38:41 +0100

    started style of registration page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,26 +5,45 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel-crm</title>
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Enriqueta:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+        {{-- styles --}}
+        <style>
+
+            body {
+                font-family: 'Enriqueta', sans-serif;
+                background-color: black;
+            }
+            .log {
+                height: 100vh;
+            }
+
+            a {
+                position: relative;
+                top: 50%;
+                left: 50%;
+                text-decoration: none;
+                color: white;
+            }
+
+        </style>
     </head>
+
     <body>
-        <div>
-            @if (Route::has('login'))
-                <div>
-                    @auth
-                        <a href="{{ url('/dashboard') }}">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}">Log in</a>
+        @if (Route::has('login'))
+            <div class="log">
+                @auth
+                    <a href="{{ url('/dashboard') }}">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}">Log in</a>
 
-                        {{-- @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif --}}
-                    @endauth
-                </div>
-            @endif
-
-
-        </div>
+                    {{-- @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif --}}
+                @endauth
+            </div>
+        @endif
     </body>
 </html>
